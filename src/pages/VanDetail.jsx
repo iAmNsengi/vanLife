74a199ee40581744655c8681b15729477990753a
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-05 15:20:03 +0200

    fix: van detail page rendering and styling

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const VanDetail = () => {
+  const { id } = useParams();
+  const [van, setVan] = React.useState([]);
+  React.useEffect(() => {
+    fetch(`api/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data));
+  }, [id]);
   return (
-    <div>
-      <h2>I am a van detailpage</h2>
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </div>
   );
 };
