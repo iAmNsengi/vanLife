f57f859e719a061c05ea143a5a1d0f4648990952
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-05 12:19:01 +0200

    feat: implemented vans page

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 
 const Vans = () => {
+  const [vans, setVans] = React.useState([]);
   React.useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setVans(data.vans));
   }, []);
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
   return (
-    <div>
-      <h2>Vans page</h2>
+    <div className="van-list-container">
+      <div className="van-list">{vanElements}</div>
     </div>
   );
 };
