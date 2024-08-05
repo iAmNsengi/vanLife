f2a5aaac1f31038be21633b6c6b40c80f3e4ed00
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-05 11:51:28 +0200

    feat: added server and mirage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import "./server";
 
 function App() {
   return (
     <BrowserRouter>
       <header>
-        <Link to={"/"}>#VANLIFE</Link>
-        <nav></nav>
+        <Link className="site-logo" to={"/"}>
+          #VANLIFE
+        </Link>
+        <nav>
+          <Link to={"/about"}>About</Link>
+        </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
-import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const Home = (props) => {
+export default function Home() {
   return (
-    <div>
-      <h1>This is home page</h1>
+    <div className="home-container">
+      <h1>You got the travel plans, we got the travel vans.</h1>
+      <p>
+        Add adventure to your life by joining the #vanlife movement. Rent the
+        perfect van to make your perfect road trip.
+      </p>
+      <Link to="vans">Find your van</Link>
     </div>
   );
-};
-
-Home.propTypes = {};
-
-export default Home;
+}
